0ca3414c36e9762f186dcfffdb790c8f4fa2782c
selee96 <pictur96@naver.com>
2024-09-02 22:56:14 +0900

    엔티티 진행중

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    private Address address;
+
+    private List<Order> orders = new ArrayList<>();
+}

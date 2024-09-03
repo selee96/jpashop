89eceb56a09cab9bbb82e9d3b8ae34acd8ef51d5
selee96 <pictur96@naver.com>
2024-09-03 20:48:39 +0900

    엔티티 진행중

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,4 +1,13 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
 public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
 }
--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -4,24 +4,22 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
-public class Member {
+public class Delivery {
 
     @Id
     @GeneratedValue
-    @Column(name = "member_id")
+    @Column(name = "derivery_id")
     private Long id;
 
-    private String name;
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
 
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READY, COMP
 }
